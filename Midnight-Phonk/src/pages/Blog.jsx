c3359437fd76264c9668c61a993380799c446347
gogux76 <se.godoyr@duocuc.cl>
2025-10-23 13:41:45 -0300

    Cambio pequeño: Agregado y modificación de los productos en el js. Cambio de descripción del blog para que se adapte más.

--- Midnight-Phonk/src/pages/Blog.jsx
+++ Midnight-Phonk/src/pages/Blog.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import PageWrapper from '../components/PageWrapper';
+import React from "react";
+import PageWrapper from "../components/PageWrapper";
 
 const Blog = () => (
   <PageWrapper title="Blog">
-    <p className="text-lg">Ayúdanos a mantener los motores encendidos. ¡Tu apoyo nos permite seguir trayéndote la mejor música!</p>
+    <p className="text-lg">
+      Aquí te traemos las mejores y más importantes noticias de nuestra
+      comunidad y mundo Phonk
+    </p>
     <button className="mt-4 bg-yellow-500 hover:bg-yellow-600 text-gray-900 font-bold py-3 px-6 rounded-full transition duration-300 shadow-lg">
       Donar Ahora
     </button>
--- Midnight-Phonk/src/utils/Phonk-Catalog.js
+++ Midnight-Phonk/src/utils/Phonk-Catalog.js
@@ -1,27 +1,28 @@
 import brazilVocalsImg from "../assets/images/brazil-vocals.png";
 import brazilSamplesImg from "../assets/images/brazilian-samples.png";
+import brazilDrumKit from "../assets/images/brazilian-phonk-drum-kit.jpg";
 
 const phonkProducts = [
   {
     id: 1,
     title: "Brazilian Vocals Volume 1",
     image: brazilVocalsImg,
-    desc: "Descripción del producto",
+    desc: "+20 vocales en diferentes formatos",
     price: 2500, //Precio del producto
   },
   {
     id: 2,
     title: "Titulo Producto",
     image: brazilSamplesImg,
-    desc: "Descripción del producto",
+    desc: "+100 samples en diferentes formatos",
     price: 2900, //Precio del producto
   },
   {
     id: 3,
     title: "Titulo Producto",
-    image: "Ruta de la imagen",
-    desc: "Descripción del producto",
-    price: 0, //Precio del producto
+    image: brazilDrumKit,
+    desc: "+30 drum kits en diferentes formatos",
+    price: 3000, //Precio del producto
   },
   {
     id: 4,
